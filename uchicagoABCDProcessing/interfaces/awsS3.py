13d410d98396cf44596e92416c6133c4a3ac3887
andrew-stier <andrewstier@uchicago.edu>
2019-11-10 22:42:20 -0600

    finish awss3

--- uchicagoABCDProcessing/interfaces/awsS3.py
+++ uchicagoABCDProcessing/interfaces/awsS3.py
@@ -1,8 +1,9 @@
 import os
+from pathlib import Path
 
 from nipype.interfaces.base import (
-    traits, TraitedSpec, BaseInterfaceInputSpec, SimpleInterface,
-    File, InputMultiPath, OutputMultiPath)
+    traits, TraitedSpec, SimpleInterface,
+    File)
 from nipype import logging
 import boto3
 import boto3.session
@@ -11,35 +12,38 @@ LOGGER = logging.getLogger('nipype.interface')
 
 
 class awsS3InputSpec(TraitedSpec):
-    link = traits.String( mandatory=False, desc='s3 link')
-    dest = traits.String( mandatory=False, desc='destination path')
-    access_key = traits.String( mandatory=False, desc='access key')
-    secret_access_key = traits.String( mandatory=False, desc='secret access key')
+    links = traits.List( mandatory=True, desc='s3 links')
+    dest = traits.String( mandatory=True, desc='destination path')
+    access_key = traits.String( mandatory=True, desc='access key')
+    secret_access_key = traits.String( mandatory=True, desc='secret access key')
+
 
 class awsS3OutputSpec(TraitedSpec):
     out_report = File(exists=True, desc='conformation report')
-    out = File(exists=True, desc='downloaded file')
+    out = traits.String(exists=True, desc='downloaded files directory')
 
 
 class s3Download(SimpleInterface):
     """
-
+        download each of the input files one at a time to the dest folder. To download files to multiple folders use
+        separate instances of this interface
     """
     input_spec = awsS3InputSpec
     output_spec = awsS3OutputSpec
 
     def _run_interface(self, runtime):
-        out=None
-        try:
-            s3 = boto3.client(
-                's3',
-                aws_access_key_id=self.inputs.access_key,
-                aws_secret_access_key=self.inputs.secret_access_key
-            )
+        s3 = boto3.client(
+            's3',
+            aws_access_key_id=self.inputs.access_key,
+            aws_secret_access_key=self.inputs.secret_access_key
+        )
+        files = []
 
-            link = self.inputs.link.split('/')
-            bucket = link[2]
-            file = self.inputs.link.split(bucket+'/')[1]
+        for link in self.inputs.links:
+            link_list = link.split('/')
+            bucket = link_list[2]
+            file = link.split(bucket+'/')[1]
+            files.append(os.path.join(self.inputs.dest,file))
             """ e.g.
             self.inputs.link = 's3://NDAR_Central_2/submission_19173/NDARINVBL4HN6F4_baselineYear1Arm1_ABCD-MPROC-DTI_20180525132204.tgz'
             link = ['s3:', '', 'NDAR_Central_2', 'submission_19173', 'NDARINVBL4HN6F4_baselineYear1Arm1_ABCD-MPROC-DTI_20180525132204.tgz']
@@ -49,12 +53,7 @@ class s3Download(SimpleInterface):
 
             s3.download_file(bucket, file, self.inputs.dest)
 
-        except Exception as e:
-            print("There was a problem with Oracle: ",  e)
-        finally:
-            if s3:
-                s3.close()
-
-        self._results['out'] = out
+        if sum([Path(f).exists for f in files]):  # only have an output if all the files were actually downloaded
+            self._results['out'] = self.inputs.dest
 
         return runtime
