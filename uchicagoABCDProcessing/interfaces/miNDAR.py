8444b2f9429ac46fed0784be374fca0b60484ace
andrew-stier <andrewstier@uchicago.edu>
2019-11-10 20:49:23 -0600

    miNDAR interface

--- uchicagoABCDProcessing/interfaces/miNDAR.py
+++ uchicagoABCDProcessing/interfaces/miNDAR.py
@@ -34,45 +34,22 @@ class miNDARQueryDFA(SimpleInterface):
     output_spec = miNDARQueryOutputSpec
 
     def _run_interface(self, runtime):
-        connection = cx_Oracle
-        img = nibabel.load(str(self.inputs.bold))
-        tr = img.header.get('pixdim')[4]
+        out=None
+        try:
+            connection: cx_Oracle.Connection = cx_Oracle.connect('%s/%s@%s' % (self.inputs.username, self.inputs.password, self.inputs.host))
+            cursor: cx_Oracle.Cursor = connection.cursor()
+            query = ''
+            cursor.execute(query)
+            rows = cursor.fetchall()
+            out = rows
+        except cx_Oracle.DatabaseError as e:
+            print("There was a problem with Oracle: ",  e)
+        finally:
+            if cursor:
+                cursor.close()
+            if connection:
+                connection.close()
 
-        use_bold = self.inputs.brainmask is not None
-
-        if use_bold:
-            mask_img = nibabel.load(str(self.inputs.brainmask))
-            data = nilearn.masking.apply_mask(imgs=img,mask_img=mask_img).T
-        else:
-            data = pandas.read_csv(str(self.inputs.csv))
-
-        data = data[:,self.inputs.drop_vols:].T
-
-        mn = int(numpy.ceil(1 / (tr * self.inputs.max_frequency)))
-        mx = int(numpy.floor(1 / (tr * self.inputs.min_frequency)))
-
-        h, hci, rs = dfa(data, max_window_size=mx, min_widow_size=mn)
-        hci = numpy.vstack(hci)
-
-        if use_bold:
-            out_hurst = fname_presuffix(self.inputs.bold, suffix='_hurst', newpath=os.getcwd())
-            out_cis = fname_presuffix(self.inputs.bold, suffix='_hurst_ci', newpath=os.getcwd())
-            out_r2s = fname_presuffix(self.inputs.bold, suffix='_hurst_r2', newpath=os.getcwd())
-
-            _bold_native_masked_derivative(bold_img=img, mask_img=mask_img, derivative_data=h, out_file=out_hurst)
-            _bold_native_masked_derivative(bold_img=img, mask_img=mask_img, derivative_data=hci[:,0], out_file=out_cis)
-            _bold_native_masked_derivative(bold_img=img, mask_img=mask_img, derivative_data=rs, out_file=out_r2s)
-        else:
-            out_hurst = fname_presuffix(self.inputs.bold, suffix='_hurst.csv', use_ext=False)
-            out_cis = fname_presuffix(self.inputs.bold, suffix='_hurst_ci.csv', use_ext=False)
-            out_r2s = fname_presuffix(self.inputs.bold, suffix='_hurst_r2.csv', use_ext=False)
-
-            numpy.savetxt(out_hurst, h, delimiter=',')
-            numpy.savetxt(out_cis, hci, delimiter=',')
-            numpy.savetxt(out_r2s, rs, delimiter=',')
-
-        self._results['hurst'] = out_hurst
-        self._results['confidence_intervals'] = out_cis
-        self._results['rsquared'] = out_r2s
+        self._results['out'] = out
 
         return runtime
