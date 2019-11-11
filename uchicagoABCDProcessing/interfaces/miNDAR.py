f66d111f3255244d47fe44731d34646f97708744
andrew-stier <andrewstier@uchicago.edu>
2019-11-10 22:31:59 -0600

    add service

--- uchicagoABCDProcessing/interfaces/miNDAR.py
+++ uchicagoABCDProcessing/interfaces/miNDAR.py
@@ -1,3 +1,6 @@
+import os
+
+import pandas
 from nipype.interfaces.base import (
     traits, TraitedSpec, BaseInterfaceInputSpec, SimpleInterface,
     File, InputMultiPath, OutputMultiPath)
@@ -7,34 +10,40 @@ import cx_Oracle
 LOGGER = logging.getLogger('nipype.interface')
 
 
-class miNDARQueryInputSpec(TraitedSpec):
+class OracleQueryInputSpec(TraitedSpec):
     subject_id = traits.String( mandatory=False, desc='subject identifier')
-    username = traits.String(mandatory=True, desc='username for miNDAR')
-    password = traits.String(mandatory=True, desc='passowrd for MINDAR')
-    host = traits.String(mandatory=True, desc='host for miNDAR')
+    username = traits.String(mandatory=True, desc='username for database')
+    password = traits.String(mandatory=True, desc='passowrd for database')
+    host = traits.String(mandatory=True, desc='host address for database')
+    service = traits.String(mandatory=False, default_value='ORCL', desc='database service')
+    query = traits.String(mandator=True, desc='oracle sql query')
 
 
-class miNDARQueryOutputSpec(TraitedSpec):
+class OracleQueryOutputSpec(TraitedSpec):
     out_report = File(exists=True, desc='conformation report')
-    out = traits.List(value=[])
+    out = File(exists=True, desc='result of database query')
 
 
-class miNDARQueryDFA(SimpleInterface):
+class OracleQuery(SimpleInterface):
     """
-
+    This is a simple interface for retrieving results of an sql query from an oracle database. The output is
     """
-    input_spec = miNDARQueryInputSpec
-    output_spec = miNDARQueryOutputSpec
+    input_spec = OracleQueryInputSpec
+    output_spec = OracleQueryOutputSpec
 
     def _run_interface(self, runtime):
-        out=None
+        out = None
+        cursor = None
+        connection = None
         try:
-            connection: cx_Oracle.Connection = cx_Oracle.connect('%s/%s@%s/ORCL' % (self.inputs.username, self.inputs.password, self.inputs.host))
+            connection: cx_Oracle.Connection = cx_Oracle.connect('%s/%s@%s/%s' % (self.inputs.username, self.inputs.password, self.inputs.host, self.inputs.service))
+            """The format for the connection string is username/password@host/service"""
             cursor: cx_Oracle.Cursor = connection.cursor()
-            query = ''
-            cursor.execute(query)
+            cursor.execute(self.inputs.query)
             rows = cursor.fetchall()
-            out = rows
+            out = pandas.DataFrame(rows)
+            out.to_csv(os.path.join(os.getcwd(), 'oracle_query_output.csv'))
+            self._results['out'] = out
         except cx_Oracle.DatabaseError as e:
             print("There was a problem with Oracle: ",  e)
         finally:
@@ -42,7 +51,5 @@ class miNDARQueryDFA(SimpleInterface):
                 cursor.close()
             if connection:
                 connection.close()
-
-        self._results['out'] = out
 
         return runtime
